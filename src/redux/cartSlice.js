6047746498366dfa19c6bea8832fe0257fc091cd
Fade1317 <f.dar1@hotmail.com>
2025-04-27 20:02:20 +0300

    Create cartSlice.js

---
+++ src/redux/cartSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  items: [],
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existingItem = state.items.find(i => i.id === item.id);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ ...item, quantity: 1 });
+      }
+    },
+    incrementQuantity: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload);
+      if (item) item.quantity += 1;
+    },
+    decrementQuantity: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload);
+      if (item && item.quantity > 1) item.quantity -= 1;
+    },
+    removeItem: (state, action) => {
+      state.items = state.items.filter(i => i.id !== action.payload);
+    },
+    clearCart: (state) => {
+      state.items = [];
+    },
+  },
+});
+
+export const { addToCart, incrementQuantity, decrementQuantity, removeItem, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
